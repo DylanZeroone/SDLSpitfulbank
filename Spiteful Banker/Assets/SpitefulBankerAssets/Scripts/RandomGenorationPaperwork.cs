8b4a2ebda45aec7ec45ee0d30d11d1f2d3fe73dd
LachlanHansfordAIE <s200014@students.aie.edu.au>
2021-07-12 11:19:29 +0930

    RandomGenoration script is work and it knows when the papper work is all good, all bad or half bad. also added test character sprites

---
+++ Spiteful Banker/Assets/SpitefulBankerAssets/Scripts/RandomGenorationPaperwork.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomGenorationPaperwork : MonoBehaviour
+{
+
+    public GameObject[] characters;
+
+    public GameObject paperworkOneGood;
+    public GameObject paperworkTwoGood;
+    public GameObject paperworkThreeGood;
+
+    public GameObject paperworkOneBad;
+    public GameObject paperworkTwoBad;
+
+    private bool _paperworkIsWrong;
+
+
+
+    public void Start()
+    {
+        ResetPaperworkAndCharacters();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ResetPaperworkAndCharacters()
+    {
+   
+        foreach (GameObject Charecter in characters)
+          {
+             Charecter.SetActive(false);
+             Debug.Log("RESET");
+
+         }
+
+    }
+
+    public void Newcharacter()
+    {
+        ResetPaperworkAndCharacters();
+
+        int characterSelection = Random.Range(0, characters.Length);
+
+        int paperworkOneSelection = Random.Range(0, 2);
+
+        int paperworkTwoSelection = Random.Range(0, 2);
+
+        if (paperworkOneSelection == 0 && paperworkTwoSelection == 0)
+        {
+            _paperworkIsWrong = false;
+            Debug.Log("everything is good");
+        }
+
+        if (paperworkOneSelection == 1 && paperworkTwoSelection == 0)
+        {
+            _paperworkIsWrong = true;
+            Debug.Log("everything is bad");
+        }
+
+        if (paperworkOneSelection == 0 && paperworkTwoSelection == 1)
+        {
+            _paperworkIsWrong = true;
+            Debug.Log("everything is bad");
+        }
+
+        if (paperworkOneSelection == 1 && paperworkTwoSelection == 1)
+        {
+            _paperworkIsWrong = true;
+            Debug.Log("everything is bad");
+        }
+
+        characters[characterSelection].SetActive(true);
+
+        if (paperworkOneSelection == 0)
+        {
+            Debug.Log("paperworkOneGood");
+
+        }
+
+        if (paperworkOneSelection == 1)
+        {
+            Debug.Log("paperworkOneBad");
+
+        }
+
+        if (paperworkTwoSelection == 0)
+        {
+            Debug.Log("paperworkTwoGood");
+
+        }
+
+        if (paperworkTwoSelection == 1)
+        {
+            Debug.Log("paperworkTwoBad");
+
+        }
+
+
+    }
+}
