b26cf8bd720f9e18482c1f094f90a65985c4a9f5
Lachlan Hansford <Lach15052003@gmail.com>
2021-07-15 03:27:42 +0930

    Added A script that takes players to a new scene depending on if their score is negitive or positive or if your score was 0. I've also change a script so you can't accept or denny loans if no one is in the room

--- Spiteful Banker/Assets/SpitefulBankerAssets/Scripts/PlayerHasSomeoneInTheRoom.cs
+++ Spiteful Banker/Assets/SpitefulBankerAssets/Scripts/PlayerHasSomeoneInTheRoom.cs
@@ -7,18 +7,24 @@ public class PlayerHasSomeoneInTheRoom : MonoBehaviour
 
 
     public GameObject doorButton;
+    public GameObject acceptButton;
+    public GameObject dennyButton;
 
     public void TalkingToSomeone()
     {
         if (gameObject.GetComponent<RandomGenorationPaperwork>().isTalking == true)
         {
             doorButton.SetActive(false);
+            acceptButton.SetActive(true);
+            dennyButton.SetActive(true);
 
         }
 
         if (gameObject.GetComponent<RandomGenorationPaperwork>().isTalking == false)
         {
             doorButton.SetActive(true);
+            acceptButton.SetActive(false);
+            dennyButton.SetActive(false);
 
             gameObject.GetComponent<RandomGenorationPaperwork>().ResetPaperworkAndCharacters();
 
