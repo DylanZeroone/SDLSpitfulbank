b9007348767286076ccabb2e50f0745faceebadf
Lachlan Hansford <Lach15052003@gmail.com>
2021-07-20 22:01:18 +0930

    Added background animations for the main menu, added scripts for the start button and the exit button

---
+++ Spiteful Banker/Assets/SpitefulBankerAssets/Scripts/ExitScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitScript : MonoBehaviour
+{
+
+    public void doExitGame()
+    {
+        Application.Quit();
+        Debug.Log("I do be quitting tho");
+    }
+
+}
---
+++ Spiteful Banker/Assets/SpitefulBankerAssets/Scripts/MainMenuCamera.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuCamera : MonoBehaviour
+{
+    public GameObject cameraOne;
+    public GameObject cameraTwo;
+    public GameObject cameraThree;
+    public GameObject cameraFour;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(MainMenuBackground());
+    }
+
+
+    public IEnumerator MainMenuBackground()
+    {
+        cameraOne.SetActive(true);
+        cameraThree.SetActive(false);
+
+        yield return new WaitForSeconds(4);
+
+        cameraFour.SetActive(true);
+        cameraOne.SetActive(false);
+
+        yield return new WaitForSeconds(4);
+
+        cameraTwo.SetActive(true);
+        cameraFour.SetActive(false);
+
+        yield return new WaitForSeconds(4);
+
+        cameraThree.SetActive(true);
+        cameraTwo.SetActive(false);
+
+        yield return new WaitForSeconds(4);
+
+        StartCoroutine(MainMenuBackground());
+
+    }
+
+}
