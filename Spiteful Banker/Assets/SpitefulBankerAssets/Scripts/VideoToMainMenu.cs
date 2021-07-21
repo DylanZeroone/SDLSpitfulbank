4e573edda23e7a0cc5a57ad4619fd5be51e32f07
Lachlan Hansford <Lach15052003@gmail.com>
2021-07-21 10:20:34 +0930

    Added all video endings into the scene

---
+++ Spiteful Banker/Assets/SpitefulBankerAssets/Scripts/VideoToMainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VideoToMainMenu : MonoBehaviour
+{
+    public float waitTime;
+
+    public string MainMenuName;
+
+    public void Start()
+    {
+        StartCoroutine(EndingCutscene());
+    }
+
+    public IEnumerator EndingCutscene()
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        SceneManager.LoadScene(MainMenuName);
+
+    }
+}
